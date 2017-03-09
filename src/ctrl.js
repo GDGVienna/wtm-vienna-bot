6acfe8b59380443846fcc8313ef185ab74248dcb
delikatessa <t.sandanova@gmail.com>
2017-03-10 00:02:35 +0100

    t

--- src/ctrl.js
+++ src/ctrl.js
@@ -24,7 +24,7 @@ exports.sendMenu = function (session) {
             buttons: [{
                 title: "Location",
                 type: "web_url",
-                url: "http://www.bing.com/maps?&cp=48.196555~16.366621&lvl=17&osid=2aee8345-1163-49ce-8df4-7b91902d8a7f&v=2&sV=2&form=S00027",
+                url: "http://maps.google.com/maps?q=48.1975989,16.367667",
                 webview_height_ratio: "full",
                 messenger_extensions: true
             }]
